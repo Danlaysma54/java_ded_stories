c42e1f5e7249a8f47295596984e094b77a3de621
Danlaysma <danlaysma45.work@gmail.com>
2023-03-29 18:28:06 +0600

    idz7_sem4

---
+++ idz7_sem4/src/main/java/InputOutput.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class InputOutput {
+    static ByteArrayOutputStream ArrayIntInBytes(int[] arr) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        for (int numbs : arr) {
+            outputStream.write(numbs);
+        }
+        outputStream.close();
+        return outputStream;
+    }
+
+    static ArrayList<Integer> BytesInIntArray(InputStream inputStream, int n) throws IOException {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        DataInputStream dataInputStream = new DataInputStream(inputStream);
+        for (int i = 0; i < n; i++) {
+            arrayList.add(dataInputStream.read());
+        }
+        return arrayList;
+    }
+
+    static ArrayList<Integer> ReaderToIntArray(Reader reader) {
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        try (Scanner scanner = new Scanner(reader)) {
+            while (scanner.hasNextInt()) {
+                arrayList.add(scanner.nextInt());
+            }
+            return arrayList;
+        }
+    }
+
+    static Writer WriterFromIntArray(int[] arr) throws IOException {
+        Writer writer = new PrintWriter(new StringWriter());
+        for (int numbs : arr) {
+            writer.write(numbs);
+        }
+        return writer;
+    }
+}
+
---
+++ idz7_sem4/src/main/java/InputOutputTest.java
@@ -0,0 +1,41 @@
+import org.junit.Test;
+
+import javax.sql.rowset.serial.SQLOutputImpl;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class InputOutputTest {
+
+    @Test
+    public void arrayIntInBytesTest() throws Exception {
+        byte[] byte_test = {1, 2, 3};
+        int[] int_test = {1, 2, 3};
+        ByteArrayOutputStream byteArrayInputStream = InputOutput.ArrayIntInBytes(int_test);
+        assertArrayEquals(byte_test, InputOutput.ArrayIntInBytes(int_test).toByteArray());
+    }
+
+    @Test
+    public void bytesInIntArrayTest() throws Exception {
+        byte[] inputArray = {1, 2, 3};
+        ArrayList<Integer> test = new ArrayList<>(List.of(1, 2, 3));
+        InputStream InputStream = new ByteArrayInputStream(inputArray);
+        assertArrayEquals(test.toArray(), InputOutput.BytesInIntArray(InputStream, 3).toArray());
+    }
+
+    @Test
+    public void ReaderToArrayIntTest() {
+        ArrayList<Integer> test1 = new ArrayList<>(List.of(1, 2, 3));
+        Reader reader = new StringReader("1 2 3");
+        assertArrayEquals(test1.toArray(), InputOutput.ReaderToIntArray(reader).toArray());
+    }
+
+    @Test
+    public void WriteFromArrayTest() throws IOException {
+        int[] int_test={1,2,3};
+        assertArrayEquals(int_test,InputOutput.WriterFromIntArray(int_test).);
+
+    }
+}
